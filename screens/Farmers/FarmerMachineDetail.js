f4c177a71cbac56595430c7bb1344a8a20edf8cc
mayur124 <mayur.chawda@somaiya.edu>
2019-06-17 14:04:14 +0530

    Farmers 50% done

--- screens/Farmers/FarmerMachineDetail.js
+++ screens/Farmers/FarmerMachineDetail.js
@@ -6,7 +6,11 @@ import {
   ActivityIndicator,
   Image,
   Dimensions,
-  TouchableOpacity
+  TouchableOpacity,
+  Modal,
+  Linking,
+  Platform,
+  Alert
 } from 'react-native';
 import { Button, Card, CardItem } from 'native-base';
 import * as firebase from 'firebase';
@@ -17,7 +21,8 @@ export default class FarmerMachineDetail extends React.Component {
     this.state = {
       adObject: {},
       vendorProfile: {},
-      price: ''
+      price: '',
+      modalVisible: false
     };
   }
 
@@ -79,6 +84,66 @@ export default class FarmerMachineDetail extends React.Component {
     return temp_image;
   };
 
+  showContactOptions = flag => {
+    this.setState({ modalVisible: flag });
+  };
+
+  whatsappAction = phone => {
+    let whatsappUrl = `whatsapp://send?text=Hello, I am ${
+      firebase.auth().currentUser.displayName
+    }. This is regarding machine ${
+      this.state.adObject.machineType
+    } which you have posted in FarmerRent&phone=91${phone}`;
+
+    Linking.openURL(whatsappUrl);
+
+    // console.log(whatsappUrl);
+    // Linking.canOpenURL(whatsappUrl)
+    //   .then(supported => {
+    //     console.log(supported);
+    //     if (supported) {
+    //       return Linking.openURL(whatsappUrl);
+    //     } else {
+    //       Alert.alert('WhatsApp is not installed');
+    //     }
+    //   })
+    //   .catch(error => console.log(error));
+  };
+
+  smsAction = phone => {
+    let phoneNumber = phone;
+    phoneNumber = `sms:${phone}`;
+
+    Linking.canOpenURL(phoneNumber)
+      .then(supported => {
+        if (!supported) {
+          Alert.alert('Phone number is not available');
+        } else {
+          return Linking.openURL(phoneNumber);
+        }
+      })
+      .catch(error => console.log(error));
+  };
+
+  callAction = phone => {
+    let phoneNumber = phone;
+    if (Platform.OS !== 'android') {
+      phoneNumber = `telprompt:${phone}`;
+    } else {
+      phoneNumber = `tel:${phone}`;
+    }
+
+    Linking.canOpenURL(phoneNumber)
+      .then(supported => {
+        if (!supported) {
+          Alert.alert('Phone number is not available');
+        } else {
+          return Linking.openURL(phoneNumber);
+        }
+      })
+      .catch(error => console.log(error));
+  };
+
   render() {
     if (
       Object.keys(this.state.adObject).length > 0 &&
@@ -87,6 +152,88 @@ export default class FarmerMachineDetail extends React.Component {
     ) {
       return (
         <View style={vendorStyles.container}>
+          <Modal
+            animationType='fade'
+            transparent={true}
+            visible={this.state.modalVisible}
+          >
+            <View
+              style={{
+                flex: 1,
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignContent: 'center'
+              }}
+            >
+              <Card
+                style={{
+                  padding: 10,
+                  width: Dimensions.get('screen').width - 10,
+                  borderRadius: 10,
+                  alignSelf: 'center'
+                }}
+              >
+                <TouchableOpacity
+                  onPress={() =>
+                    this.showContactOptions(!this.state.modalVisible)
+                  }
+                >
+                  <Text
+                    style={{
+                      right: 0,
+                      fontSize: 20,
+                      fontWeight: 'bold'
+                    }}
+                  >
+                    X
+                  </Text>
+                </TouchableOpacity>
+                <CardItem
+                  style={{
+                    justifyContent: 'space-around'
+                  }}
+                >
+                  <TouchableOpacity
+                    style={{ flexDirection: 'column' }}
+                    onPress={() =>
+                      this.callAction(this.state.vendorProfile[0].phone)
+                    }
+                  >
+                    <Image
+                      source={require('../../assets/call-answer.png')}
+                      style={{ height: 50, width: 50 }}
+                    />
+                    <Text>Call</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    style={{ flexDirection: 'column' }}
+                    onPress={() =>
+                      this.smsAction(this.state.vendorProfile[0].phone)
+                    }
+                  >
+                    <Image
+                      source={require('../../assets/speech-bubble.png')}
+                      style={{ height: 50, width: 50 }}
+                    />
+                    <Text>SMS</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    style={{ flexDirection: 'column' }}
+                    onPress={() =>
+                      this.whatsappAction(this.state.vendorProfile[0].phone)
+                    }
+                  >
+                    <Image
+                      source={require('../../assets/whatsapp.png')}
+                      style={{ height: 50, width: 50 }}
+                    />
+                    <Text>WhatsApp</Text>
+                  </TouchableOpacity>
+                </CardItem>
+              </Card>
+            </View>
+          </Modal>
+
           <ScrollView
             style={{ marginBottom: 60 }}
             contentContainerStyle={{
@@ -124,7 +271,13 @@ export default class FarmerMachineDetail extends React.Component {
                 </View>
                 <View style={{ flexDirection: 'row' }}>
                   <Text style={{ fontWeight: '700' }}>Phone: </Text>
-                  <Text>{this.state.vendorProfile[0].phone}</Text>
+                  <TouchableOpacity
+                    onPress={() => this.showContactOptions(true)}
+                  >
+                    <Text style={{ color: '#2475B0' }}>
+                      {this.state.vendorProfile[0].phone}
+                    </Text>
+                  </TouchableOpacity>
                 </View>
                 <View style={{ flexDirection: 'row', flexWrap: 'wrap' }}>
                   <Text style={{ fontWeight: '700' }}>Address: </Text>
